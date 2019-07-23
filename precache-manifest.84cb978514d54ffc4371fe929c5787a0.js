cba36ad13270bb4197447183ae8f90bcc66325f1
SofiaRuizHinojosa27 <srh_19@hotmail.com>
2019-07-22 19:41:39 -0500

    Updates

---
+++ precache-manifest.84cb978514d54ffc4371fe929c5787a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d54e1b7f8def2812c16509349864bb7",
+    "url": "/sofyruizportafolio/./index.html"
+  },
+  {
+    "revision": "7bf74535d508c6dc1305",
+    "url": "/sofyruizportafolio/./static/css/main.0ae3661e.chunk.css"
+  },
+  {
+    "revision": "14f4368826b7efded6bf",
+    "url": "/sofyruizportafolio/./static/js/2.f78b363c.chunk.js"
+  },
+  {
+    "revision": "7bf74535d508c6dc1305",
+    "url": "/sofyruizportafolio/./static/js/main.36bc9281.chunk.js"
+  },
+  {
+    "revision": "96d7cdf73f67fd623b86",
+    "url": "/sofyruizportafolio/./static/js/runtime~main.f76b0e95.js"
+  },
+  {
+    "revision": "7d37bfe4138833c109038d9345c92cb3",
+    "url": "/sofyruizportafolio/./static/media/Sofi3.7d37bfe4.jpg"
+  },
+  {
+    "revision": "5c360accb03b02d9f7a1754979bfe7ee",
+    "url": "/sofyruizportafolio/./static/media/logo2.5c360acc.jpg"
+  }
+]);
